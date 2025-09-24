c17b3fc42f90db912dcda005a4448a36969969ec
Melusi <moyomelusi65@gmail.com>
2025-09-24 23:06:47 +1000

    Add updated code

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool oTurn = true;
   List<String> displayElement = ['', '', '', '', '', '', '', '', ''];
   int oScore = 0;
   int xScore = 0;
@@ -21,50 +22,235 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: <Widget>[
           Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text("Player X", style: CustomTextStyle.playerInfo),
+                          Text(
+                            xScore.toString(),
+                            style: CustomTextStyle.playerInfo,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text("Player O", style: CustomTextStyle.playerInfo),
+                          Text(
+                            oScore.toString(),
+                            style: CustomTextStyle.playerInfo,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    margin: EdgeInsets.only(bottom: 20.0),
+                    decoration: BoxDecoration(color: Colors.amber),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Turn: ${oTurn ? 'O' : 'X'}",
+                      style: CustomTextStyle.playerInfo,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: GridView.builder(
+              itemCount: 9,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    _tapped(index);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                    ),
+                    child: Text(
+                      textAlign: TextAlign.center,
+                      displayElement[index],
+                      style: TextStyle(color: Colors.white, fontSize: 35),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
             child: Container(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(30.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text("Player X", style: CustomTextStyle.playerInfo),
-                        Text(
-                          xScore.toString(),
-                          style: CustomTextStyle.playerInfo,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(30.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text("Player O", style: CustomTextStyle.playerInfo),
-                        Text(
-                          oScore.toString(),
-                          style: CustomTextStyle.playerInfo,
-                        ),
-                      ],
+                  ElevatedButton(
+                    onPressed: () {
+                      _clearScoreBoard();
+                    },
+                    child: Text("Clear Score Board"),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.red,
                     ),
                   ),
                 ],
               ),
             ),
           ),
-          Expanded(
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-              ),
-              itemBuilder: (BuildContext context, int index) {},
-            ),
-          ),
         ],
       ),
     );
+  }
+
+  void _checkWinner() {
+    // Checking rows
+    if (displayElement[0] == displayElement[1] &&
+        displayElement[0] == displayElement[2] &&
+        displayElement[0] != '') {
+      _showWinDialog(displayElement[0]);
+    }
+    if (displayElement[3] == displayElement[4] &&
+        displayElement[3] == displayElement[5] &&
+        displayElement[3] != '') {
+      _showWinDialog(displayElement[3]);
+    }
+    if (displayElement[6] == displayElement[7] &&
+        displayElement[6] == displayElement[8] &&
+        displayElement[6] != '') {
+      _showWinDialog(displayElement[6]);
+    }
+
+    // Checking Column
+    if (displayElement[0] == displayElement[3] &&
+        displayElement[0] == displayElement[6] &&
+        displayElement[0] != '') {
+      _showWinDialog(displayElement[0]);
+    }
+    if (displayElement[1] == displayElement[4] &&
+        displayElement[1] == displayElement[7] &&
+        displayElement[1] != '') {
+      _showWinDialog(displayElement[1]);
+    }
+    if (displayElement[2] == displayElement[5] &&
+        displayElement[2] == displayElement[8] &&
+        displayElement[2] != '') {
+      _showWinDialog(displayElement[2]);
+    }
+
+    // Checking Diagonal
+    if (displayElement[0] == displayElement[4] &&
+        displayElement[0] == displayElement[8] &&
+        displayElement[0] != '') {
+      _showWinDialog(displayElement[0]);
+    }
+    if (displayElement[2] == displayElement[4] &&
+        displayElement[2] == displayElement[6] &&
+        displayElement[2] != '') {
+      _showWinDialog(displayElement[2]);
+    } else if (filledBoxes == 9) {
+      _showDrawDialog();
+    }
+  }
+
+  void _tapped(int index) {
+    setState(() {
+      if (oTurn && displayElement[index] == '') {
+        displayElement[index] = 'O';
+        filledBoxes++;
+      } else if (!oTurn && displayElement[index] == '') {
+        displayElement[index] = 'X';
+        filledBoxes++;
+      }
+      oTurn = !oTurn;
+      _checkWinner();
+    });
+  }
+
+  void _showWinDialog(String winner) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("\" " + winner + " \" is Winner!!!"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                _clearBoard();
+                Navigator.of(context).pop();
+              },
+              child: Text("Play Again!"),
+              style: TextButton.styleFrom(foregroundColor: Colors.red),
+            ),
+          ],
+        );
+      },
+      barrierDismissible: false,
+    );
+    if (winner == 'O') {
+      oScore++;
+    } else if (winner == 'X') {
+      xScore++;
+    }
+  }
+
+  void _showDrawDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Draw!"),
+          actions: [
+            TextButton(
+              onPressed: () {
+                _clearBoard();
+                Navigator.of(context).pop();
+              },
+              child: Text("Play Again!"),
+              style: TextButton.styleFrom(foregroundColor: Colors.red),
+            ),
+          ],
+        );
+      },
+      barrierDismissible: false,
+    );
+  }
+
+  void _clearBoard() {
+    setState(() {
+      displayElement.clear();
+      displayElement.addAll(['', '', '', '', '', '', '', '', '']);
+    });
+    filledBoxes = 0;
+  }
+
+  void _clearScoreBoard() {
+    setState(() {
+      xScore = 0;
+      oScore = 0;
+      displayElement.clear();
+      displayElement.addAll(['', '', '', '', '', '', '', '', '']);
+    });
+
+    filledBoxes = 0;
   }
 }
